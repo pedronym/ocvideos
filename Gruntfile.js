0139b3b0437da6c26cc92b4321fb1eb89df203d2
Pedro Lima <plima@parceiro.sonae.pt>
2016-04-04 02:39:55 +0100

    Doing it right. Working on a way to precompile handlebars templates and to simply use a json file to create all the site's info making it easier to update.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,48 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    dirs: {
+      templates:  'public/templates',
+      sass:       'public/sass',
+      css:        'public/css',
+      scripts:    'public/js',
+      data:       'public/data'
+    },
+    handlebars: {
+        all: {
+            files: {
+                '<%= dirs.scripts %>/templates.js': ['<%= dirs.templates %>/intro.handlebars', '<%= dirs.templates %>/project.handlebars', '<%= dirs.templates %>/footer.handlebars']
+
+            }
+        }
+    },
     sass: {
         options: {
-            sourceMap: true
+            sourcemap: true,
+            style: 'expanded'
         },
-        dist: {
-            files: {
-                'public/css/main.css': 'public/sass/main.scss'
-            }
+        files: {
+            '<%= dirs.css %>/main.css': '<%= dirs.sass %>/main.scss'
         }
     },
     watch: {
         sass: {
-        files: ['public/**/*.scss'],
-        tasks: ['sass'],
-        options: {
-          spawn: false,
-          livereload: true
-          }
+            files: ['<%= dirs.sass %>/**.scss'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+                livereload: true
+            }
+        },
+        handlebars: {
+            files: ['<%= dirs.templates %>/**.handlebars'],
+            tasks: ['handlebars:all']
         }
     }
   });
-  
-  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-handlebars-compiler');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('dev', ['watch']);
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	console.log('Main Running');
 
-	$('.social-links-item').each(function(){
-
+	console.log(Handlebars.templates);
+	Handlebars.templates['intro']({
+		title: 'Bem vindo à página da OC Videos®',
+		text:  'O meu nome é Óscar Chaves, estudei Som e Imagem na Universidade Católica Portuguesa, vivo no Porto e adoro idealizar e criar os meus próprios vídeos. Alguns dos meus trabalhos foram realizados em eventos de empresas como Prio, Sonae, Câmara Municipal de Gaia, Sodecia e Team Building. Procuro acima de tudo captar a essência de cada momento para que as pessoas se possam rever no resultado final.'
 	});
-
 });
