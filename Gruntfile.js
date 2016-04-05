2a3a6c3bbfb5ea5b2ce2cbffc43d2dc03e14753e
Pedro Lima <plima@parceiro.sonae.pt>
2016-04-05 02:04:32 +0100

    Made the intro text 100% container width.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,67 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    dirs: {
+      templates:  'public/templates',
+      sass:       'public/sass',
+      css:        'public/css',
+      scripts:    'public/js',
+      data:       'public/data'
+    },
+    express: {
+        dev: {
+            options: {
+                port: 8080,
+                script: 'index.js',
+            }
+        }
+    },
+    handlebars: {
+        all: {
+            files: {
+                '<%= dirs.scripts %>/templates.js': '<%= dirs.templates %>/**.handlebars'
+            }
+        }
+    },
     sass: {
         options: {
-            sourceMap: true
+            sourcemap: true,
+            style: 'expanded'
         },
-        dist: {
-            files: {
-                'public/css/main.css': 'public/sass/main.scss'
-            }
+        files: {
+            '<%= dirs.css %>/main.css': '<%= dirs.sass %>/main.scss'
         }
     },
     watch: {
         sass: {
-        files: ['public/**/*.scss'],
-        tasks: ['sass'],
-        options: {
-          spawn: false,
-          livereload: true
-          }
+            files: ['<%= dirs.sass %>/**.scss'],
+            tasks: ['sass'],
+            spawn: false,
+            options: {
+                livereload: {
+                    host: 'localhost',
+                    port: 9000,
+                }
+            }
+        },
+        handlebars: {
+            files: ['<%= dirs.templates %>/**.handlebars'],
+            tasks: ['handlebars:all']
+        },
+        express: {
+            files:   ['<%= dirs.scripts %>/**.js'],
+            tasks:   ['express:dev'],
+            options: {
+                spawn: false
+            }
         }
     }
   });
-  
+
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-handlebars-compiler');
+  grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('dev', ['express:dev', 'watch']);
 }
